3dee20948de940256a29007969ebcc99dd285bf2
dex <arash@OS.local>
2024-12-29 19:00:52 +0330

    add new config

---
+++ Config/colors.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Gruvbox Dark (Hard) Color Palette
+
+export COLOR_BACKGROUND=0xe0000000
+export COLOR_FOREGROUND=0xe0ffffff
+
+export COLOR_BLACK=xe0000000
+export COLOR_WHITE=xe0ffffff
